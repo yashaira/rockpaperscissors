3541e9da29efaa4c26ded2051e4b2bebf5a1403a
Yashaira Longras <ylongras@gmail.com>
2018-11-05 23:17:40 +0000

    first draft

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@
 /* global $ */
 
 
+var computer_input= Math.floor((Math.random() * 3) + 1)
+
+$("#shoot").click(function() { 
+    var user_input = $("#input").val(); 
+$("#userChoice").text(user_input);
+
+ if(computer_input==1){
+ computer_input="rock";
+}
+else if (computer_input==2){
+ computer_input="paper";
+}
+else{ 
+    computer_input= "scissors"; 
+}
+
+$("#computerChoice").text(computer_input); 
+
+});
+
 
 // DOCUMENT READY FUNCTION BELOW
 
